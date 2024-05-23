d172d388847692a2e414c087dca5df35a0cb8857
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-05-24 10:57:49 +1200

    tested database

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-print("Hello World")
+'''Docstring This is a python and sql code that hasthe information of Movie database
+By Aaron Choi on 24.05.2024'''
+
+import sqlite3
+
+db = sqlite3.connect("movie_database.db")
+cursor= db.cursor()
+sql = "SELECT * from Movie;"
+cursor.execute(sql)
+results = cursor.fetchall
+print(results)
+
+db.close()
