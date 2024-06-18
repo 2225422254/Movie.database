acd48c67558e0c5e0f07509fd89111eae19866f4
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-06-19 11:52:06 +1200

    Added a code for the person to put an input

--- app.py
+++ app.py
@@ -108,11 +108,67 @@ def print_all_film_joint_with_Genre():
     #loop finished here
     db.close()
 
+
+def print_all_film_rating_9():
+    db = sqlite3.connect(database)
+    cursor = db.cursor()
+    sql = "SELECT Movie.Movie_name,Movie.Rating, Movie.Release_date, Genre.name FROM Movie JOIN Genre ON Movie.Genre_id=Genre.Genre_id;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    #loop through all the results
+    print(f"Movie_name                                             Rating    Release_date   Genre_name")
+    for Movie in results:
+        print(f"{Movie[0]:<55}{Movie[1]:<10}{Movie[2]:<15}{Movie[3]:<1}")
+    #loop finished here
+    db.close()
+
+
+def Release_date_input(releasedatename):
+    db = sqlite3.connect(database)
+    cursor = db.cursor()
+    sql = "SELECT Movie_name, Rating, Release_date from Movie Where Movie.Release_date ="+releasedatename+";"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    #loop through all the results
+    print(f"Movie_name                                           Rating    Release_date")
+    for Movie in results:
+        print(f"{Movie[0]:<53}{Movie[1]:<10}{Movie[2]:<16}")
+    #loop finished here
+    db.close()
+
+
+def Rating_input(ratingname):
+    db = sqlite3.connect(database)
+    cursor = db.cursor()
+    sql = "SELECT Movie_name, Rating, Release_date from Movie Where Movie.Rating ="+ratingname+";"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    #loop through all the results
+    print(f"Movie_name                                           Rating    Release_date")
+    for Movie in results:
+        print(f"{Movie[0]:<53}{Movie[1]:<10}{Movie[2]:<16}")
+    #loop finished here
+    db.close()
+
+
+def Genre_input(genrename):
+    db = sqlite3.connect(database)
+    cursor = db.cursor()
+    sql = "SELECT Movie.Movie_name,Movie.Rating, Movie.Release_date, Genre.name FROM Movie JOIN Genre ON Movie.Genre_id=Genre.Genre_id WHERE Genre.name ="+genrename+";"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    #loop through all the results
+    print(f"Movie_name                                             Rating    Release_date   Genre_name")
+    for Movie in results:
+        print(f"{Movie[0]:<55}{Movie[1]:<10}{Movie[2]:<15}{Movie[3]:<1}")
+    #loop finished here
+    db.close()
+
 #main code
 while True:
-    user_input = input("\nWhat would you like to do in this Movie Database?\nEnter '1' to see all information\nEnter '2' to sort the data\nEnter '3' to see the specific data\nEnter '4' to Exit the page\n")
+    user_input = input("\nWhat would you like to do in this Movie Database?\nEnter '1' to see all information\nEnter '2' to sort the data\nEnter '3' to see the data with your input\nEnter '0' to Exit the page\n")
     if user_input == "1":
-        user_input_information = input("What information would you like to see?\nEnter '1' to see all Movie name\nEnter '2' to see all Genre name\nEnter '3' to see all Movie database\nEnter '4' to see all Movie and Genre database\nEnter '5' to Exit the page\n")
+        user_input_information = input("What information would you like to see?\nEnter '1' to see all Movie name\nEnter '2' to see all Genre name\nEnter '3' to see all Movie database\nEnter '4' to see all Movie and Genre database\nEnter '0' to Exit the page\n")
         if user_input_information == '1':
             print_all_Movie_name()
         elif user_input_information == '2':
@@ -121,25 +177,36 @@ while True:
             print_all_film()
         elif user_input_information == "4":
             print_all_film_joint_with_Genre()
-        elif user_input_information == "5":
+        elif user_input_information == "0":
             continue
         else:
             print("That is not a vailid option")
     elif user_input == "2":
-        user_input_sort = input("What data would you like to sort by?\nEnter '1' to sort by Movie name\nEnter '2' to sort by Rating\nEnter '3' to sort by Release date\nEnter '4' to Exit the page\n")
+        user_input_sort = input("What data would you like to sort by?\nEnter '1' to sort by Movie name\nEnter '2' to sort by Rating\nEnter '3' to sort by Release date\nEnter '0' to Exit the page\n")
         if user_input_sort == "1":
             print_all_film_sorted_by_Movie_name()
         elif user_input_sort == "2":
             print_all_film_sorted_by_Rating()
         elif user_input_sort == "3":
             print_all_film_sorted_by_Release_date()
-        elif user_input_sort == '4':
+        elif user_input_sort == '0':
             continue
         else:
             print("That is not a valid option")
     elif user_input == '3':
-        user_input_specific = input("What data would you like to specificly see?\nEnter '1' to see all Movie with a Rating on 8\nEnter '2' to see all Movie with a Rating on 9\nEnter '3' to see all Movie that is made before the year 2000\nEnter '4' to see all Movie that is made in betweeen the year 2000 to 2010\nEnter '5' to see all Movie that is made in between the year 2010 ro 2020\n")
-    elif user_input == "4":
+        user_input_input = input("What data would you like to see with your input?\nEnter '1' to input a Rating\nEnter '2' to input a Release date\nEnter '3' to input a Genre\nEnter '0' to Exit the page\n")
+        if user_input_input == '1':
+            Rating = input("Which Rating would you like to see between 1 to 10 with your input?\n")
+            Rating_input(Rating)
+        elif user_input_input == '2':
+            Release_date = input("Which Release date would you like to see with you input?\n")
+            Release_date_input(Release_date)
+        elif user_input_input == '3':
+            Genre = input("Which genre would you like to see with your input?\n")
+            Genre_input(Genre)
+        else:
+            print("That is not a vailid option")
+    elif user_input == "0":
         print("Goodbye")
         break
     else:
